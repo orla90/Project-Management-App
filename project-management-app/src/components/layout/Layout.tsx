7a34af3ca1a3e98d15288cb51ab8b87c88af8eb1
OstapchelaEst <Ostapchela@gmail.com>
2022-11-10 10:36:03 +0100

    fix: remove comments

--- project-management-app/src/components/layout/Layout.tsx
+++ project-management-app/src/components/layout/Layout.tsx
@@ -1,6 +1,4 @@
-//import { CustomLink } from 'components/UI/custom-link/CustomLink';
 import Header from 'components/UI/header/header';
-//import { ROUTES } from 'constants/routes';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
