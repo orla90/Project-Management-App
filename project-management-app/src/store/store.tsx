e9f5f26646db1079ce7376436b524150a1995172
OstapchelaEst <Ostapchela@gmail.com>
2022-11-12 22:38:57 +0100

    feat: switcher language

--- project-management-app/src/store/store.tsx
+++ project-management-app/src/store/store.tsx
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import languageSlice from './slices/language-slice';
 import signSlice from './slices/sign-slice';
+
 export const rootReducer = combineReducers({
   signSlice,
+  languageSlice,
 });
 
 export const setupStore = () => {
