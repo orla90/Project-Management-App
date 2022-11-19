dc8b5288e37b7a4318d9ea32f3fe0178cad0c692
lenaStsk <staskevichalena@gmail.com>
2022-11-19 18:30:27 +0300

    feat: show  all created boards

--- project-management-app/src/pages/boards-list-page/components/BoardList.tsx
+++ project-management-app/src/pages/boards-list-page/components/BoardList.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BoardItem from './BoardItem';
+import './board-list.scss';
+import BoardItemAdd from './BoardItemAdd';
+import { getBoardsByUserIdFetch } from '../../../store/actions-creators/boards/boards-action';
+import { useAppDispatch, useAppSelector } from 'store/custom-hooks';
+import { IBoard } from './interfaces/IBoard';
 
 const BoardList = () => {
-  return <div className="board-list-container"></div>;
+  const [boards, setBoards] = useState<IBoard[]>([]);
+
+  const { user } = useAppSelector((state) => state.signSlice);
+  const dispatch = useAppDispatch();
+
+  const loadBoards = async () => {
+    try {
+      const userBoards = await dispatch(getBoardsByUserIdFetch({ userId: user!.id })).unwrap();
+      setBoards(userBoards);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    loadBoards();
+  });
+
+  return (
+    <div className="board-list-container">
+      {boards.map((board) => (
+        <BoardItem board={board} key={board._id} />
+      ))}
+      <BoardItemAdd />
+    </div>
+  );
 };
 
 export default BoardList;
